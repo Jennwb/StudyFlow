f0ae35bd2a7105f64bfd4ab90034d57736973d98
Jennifer <jenniferbeatrizmn@gmail.com>
2021-09-21 20:24:45 -0300

    Criando "login.html"

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template
 from app import app
+from flask import render_template
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
     nome = "Jennifer"
-    return render_template('index.html', title='Página Inicial', nome=nome)
+    return render_template('index.html', title='StudyFlow', nome=nome)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    return "{} e {}".format(usuario, senha)
