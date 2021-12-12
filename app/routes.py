2446612719f8122dc2aad4f6f77da8395f78e107
glooooh <glorinhamsouza@gmail.com>
2021-12-12 14:31:24 -0300

    Rascunho CRUD Lembretes

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,10 @@ from app import app
 from flask import render_template
 from flask import request, session
 from flask import flash, redirect, url_for
-from app.forms import LoginForm, RegistrarForm, AdicionarMaterias, EditarMaterias
+from app.forms import LoginForm, RegistrarForm, AdicionarMaterias, EditarMaterias, AdicionarLembretes
 from app.models.usuario import Usuario
 from app.models.materia import Materia
+from app.models.lembrete import Lembrete
 from app import db, lm
 from flask_login import login_user, login_required, current_user
 import bcrypt
@@ -13,23 +14,17 @@ from app import conexao
 import sys
 
 # User Loader
-
-
 @lm.user_loader
 def load_user(id_usuario):
     return Usuario.query.filter_by(id_usuario=id_usuario).first()
 
 # Index
-
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Study Flow')
 
 # Login
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -63,8 +58,6 @@ def login():
     return render_template('login.html', form=form)
 
 # Cadastro
-
-
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
     form = RegistrarForm()
@@ -95,16 +88,12 @@ def registrar():
     return render_template("registrar.html", form=form)
 
 # Logout
-
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect('/')
 
 # Home
-
-
 @app.route('/home')
 def home():
     if not current_user.is_authenticated:
@@ -114,8 +103,6 @@ def home():
         return render_template('home.html', title='Study Flow')
 
 # Matérias
-
-
 @app.route('/materias')
 def materias():
     if not current_user.is_authenticated:
@@ -127,8 +114,6 @@ def materias():
         return render_template('materias/listar_materia.html', title='Matérias', materias_db=materias_db)
 
 # Matérias - adicionar
-
-
 @app.route('/materias/adicionar', methods=['GET', 'POST'])
 def adicionar():
     if not current_user.is_authenticated:
@@ -225,3 +210,112 @@ def excluir(codMateria):
 
 			# Tem que colocar a mensagem de erro por causa da chave estrangeira do ciclo
 			return (redirect("/materias"))
+
+# Lembretes
+@app.route('/lembretes')
+def lembretes():
+    if not current_user.is_authenticated:
+        flash('Apressadinho! Logue na sua conta primeiro.', 'warning')
+        return redirect('/login')
+    else:
+        id_usuario = current_user.get_id()
+        lembretes = Lembrete.query.filter_by(id_usuario=id_usuario).all()
+        return render_template('lembretes/listar_lembretes.html', title='Lembretes', lembretes=lembretes)
+
+# Lembretes - adicionar
+@app.route('/lembretes/adicionar', methods=['GET', 'POST'])
+def adicionarL():
+	if not current_user.is_authenticated:
+		flash('Apressadinho! Logue na sua conta primeiro.', 'warning')
+		return redirect('/login')
+	else:
+		form = AdicionarLembretes()
+		if form.validate_on_submit():
+			id_usuario = current_user.get_id()
+			tipo = form.tipo.data
+			nome = form.nome.data
+			descricao = form.descricao.data
+			data_hora = form.data_hora.data
+
+			# Executa o comando:
+			lembrete = Lembrete(id_usuario, tipo, nome, descricao, data_hora)
+
+			# Efetua um commit no banco de dados.
+			# Por padrão, não é efetuado commit automaticamente. Você deve commitar para salvar
+			# suas alterações.
+			db.session.add(lembrete)
+
+			# Aqui pode pedir uma confirmação
+			db.session.commit()
+
+			flash("Lembrete registrado com sucesso!", "success")
+			return (redirect("/lembretes"))
+
+		elif len(form.errors.items()) > 0:
+			for campo, mensagens in form.errors.items():
+				for m in mensagens:
+					flash(m, "danger")
+			return (redirect("/lembretes/nova_materia.html"))
+
+		return render_template('lembretes/nova_materia.html', form=form)
+
+# Lembretes - editar ----- NÃO PRONTO
+@app.route('/editar/<codLembrete>', methods=['GET', 'POST'])
+def editarL(codLembrete):
+	if not current_user.is_authenticated:
+		flash('Apressadinho! Logue na sua conta primeiro.', 'warning')
+		return redirect('/login')
+	else:
+		codLembrete = codLembrete
+		materia = Materia.query.filter_by(codLembrete=codLembrete).first()
+		id = materia.id_usuario
+		if (current_user.get_id() == id):
+			form = EditarMaterias()
+			if form.validate_on_submit():
+				# (materia.nome).update(dict(nome=form.nome.data))
+				# (materia.nivelAfinidade).update(dict(nivelAfinidade=form.nivel_afinidade.data))
+				# (materia.pesoProva).update(dict(pesoProva=form.peso_prova.data))
+
+				materia = Materia.query.filter_by(codLembrete=codLembrete).first()
+				materia.nome = form.nome.data
+				# db.session.commit()
+				materia.nivelAfinidade = form.nivel_afinidade.data
+				# db.session.commit()
+				materia.pesoProva = form.peso_prova.data
+				# db.session.commit()
+
+				# num_rows_updated = Materia.query.filter_by(codLembrete=codLembrete).update(dict(nome = form.nome.data, nivelAfinidade = form.nivel_afinidade.data, pesoProva = form.peso_prova.data))
+				db.session.merge(materia)
+				db.session.flush()
+				db.session.commit()
+
+	            # Aqui pode pedir uma confirmação
+				
+
+				flash("Matéria registrada com sucesso!", "success")
+				return (redirect("/lembretes"))
+
+			elif len(form.errors.items()) > 0:
+				for campo, mensagens in form.errors.items():
+					for m in mensagens:
+						flash(m, "danger")
+				return (redirect("/lembretes/editar_materia.html"))
+
+			return render_template('/lembretes/editar_materia.html', form=form, materia=materia)
+
+# Lembretes - excluir
+@app.route('/excluir/<codLembrete>', methods=['GET', 'POST'])
+def excluir(codLembrete):
+	if not current_user.is_authenticated:
+		flash('Apressadinho! Logue na sua conta primeiro.', 'warning')
+		return redirect('/login')
+	else:
+		codLembrete = codLembrete
+		lembrete = Lembrete.query.filter_by(codLembrete=codLembrete).first()
+		id = lembrete.id_usuario
+		if (current_user.get_id() == id):
+			db.session.delete(lembrete)
+			db.session.commit()
+
+			# Tem que colocar a mensagem de erro por causa da chave estrangeira do ciclo
+			return (redirect("/lembretes"))
